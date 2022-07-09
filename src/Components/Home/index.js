7ad11d6856ec5217975903ffdc9ff3a33d12588f
unknown <agnidipto12.25@gmail.com>
2022-07-09 20:57:07 +0530

    Added Pagination

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,34 +1,111 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {Button,
+Form,
 Container,
 Row,
-Col} from 'react-bootstrap';
+Col,
+Navbar} from 'react-bootstrap';
+// import Flippy, {FrontSide, BackSide} from 'react-flippy';
+import ReactPaginate from 'react-paginate';
 import ReactCardFlip from "react-card-flip";
+import ExampleModal from "../ExampleModal";
 
-const words = require("../../index.json");
+const words = require("../../output.json");
+
 
 function Home(props) {
 
     const [flipped, setFlipped]=useState('xyz');
 
+    const [currentItems, setCurrentItems] = useState(words.slice(0,10)); // for items to be shown in the page
+    const [pageCount, setPageCount] = useState(0); // to count the total number of pages
+    const [itemOffset, setItemOffset] = useState(0); // for the first item in each page / first item in currentItems
+    const [itemsPerPage, setItemsPerPage] = useState(10); // how many items per page
+    const [pageSelected, setPageSelected]= useState(1);
+    
+    const [modalShow, setModalShow] = useState(false);
+    const [modalWord, setModalWord] = useState('xyz');
+    const [modalExample, setModalExample] = useState([]);  
+
+
+
+    useEffect(() => {
+        const endOffset= itemOffset + parseInt(itemsPerPage); // last item in the page + 1
+        console.log('offset'+itemOffset + 'endoffset' + endOffset);
+        setCurrentItems(words.slice(itemOffset, endOffset)); // slicing the array
+        setPageCount(Math.ceil(words.length / itemsPerPage)); // setting the Page Count
+    }, [itemsPerPage, itemOffset]);
+
+    const handlePageClick = (event) => {
+        const newOffset = (event.selected * parseInt(itemsPerPage)) % words.length; // triggers everytime page is clicked on
+        setPageSelected(event.selected);
+        setItemOffset(newOffset); // set new Item Offset 
+    }
+
+    const changeWordsPerPage = (e) => {
+        setItemsPerPage(e.target.value);
+        const newOffset = (pageSelected * parseInt(e.target.value)) % words.length; // triggers everytime page is clicked on
+        setItemOffset(newOffset); // set new Item Offset 
+        
+    }
+
+    
+
     return (
         <>
         <div>
-            <h1>GRE English flashcards</h1>
-            <h2>Total words: {words.length}</h2>
-            <Container>
-                <Row>
 
-                    {words.map(x => 
-                        <Col sm='6' md='4'
+            <Navbar bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">
+                
+            GRE English Flashcards
+            </Navbar.Brand>
+            </Container>
+            </Navbar>
+            <h2
+            style={{margin:"20px 0"}}>Total words: {words.length}</h2>
+            
+            <div style={{margin:'auto', maxWidth:"300px"}}>
+            <Form>
+                <Form.Label>No. of words per page</Form.Label>
+            <Form.Select aria-label="Default select example"
+            onChange={changeWordsPerPage}
+            value={itemsPerPage}>
+                <option value='5'>5</option>
+                <option value="10" default>10</option>
+                <option value="20">20</option>
+                <option value="50">50</option>
+                <option value="100">100</option>
+            </Form.Select>
+            </Form>
+            </div>
+
+
+            <Container>
+                <div className='box'>
+                <Row>
+                    {currentItems.map(x => 
+                        <Col sm='6' md='4' xs='6' lg='3'
                         style={{
                             marginTop:20
                         }}>
+
+
+                        {/* <Flippy
+                            flipOnHover={false}
+                            flipOnClick={true}
+                            flipDirection = 'horizontal'
+                            style={{ height: '200px', maxWidth:'300px'}}>
+                                <FrontSide className="frontcard">
+                                    {x['word']}</FrontSide>
+                                <BackSide className='backcard'>
+                                    {x['meaning']}</BackSide>
+                        </Flippy> */}
+
                         <ReactCardFlip isFlipped={flipped === x['word']}
-                        cardStyles={{"front":{"height" : 150,
-                                    "backgroundColor" : "#91f0ff"},
-                                "back": {"height" : 150,
-                                "backgroundColor" : "#86ffc5"}}}
+                        cardStyles={{"front":{"height" : 200},
+                                "back": {"height" : 200}}}
                             flipDirection='horizontal'>
                             <div onClick={() => {
                                 setFlipped(x['word']);
@@ -36,7 +113,7 @@ function Home(props) {
                             className="frontcard"
                             >
                                 <div classname='eachcard'>
-                                <p >{x['word']}</p>
+                                <p >{x['word'].charAt(0).toUpperCase() + x['word'].slice(1)}</p>
                                 </div>
                             </div>
                             <div onClick={() => {
@@ -46,14 +123,51 @@ function Home(props) {
                             >
                                 <div classname='eachcard'>
                                 <p >{x['meaning']}</p>
+                                {x['examples'][0] ? <Button
+                                onClick={(e) => {
+                                    setModalWord(x['word'].charAt(0).toUpperCase() + x['word'].slice(1));
+                                    setModalExample(x['examples']);
+                                    setModalShow(true);
+                                    console.log(modalExample)
+                                }}
+                                variant='info'>+</Button> : null}
+                                
                                 </div>
                             </div>
                         </ReactCardFlip>
                         </Col>
                     )}
                 </Row>
+                </div>
+
+                <hr />
+                <div style={{marginTop:30, 
+                display: 'block',
+                marginBottom: 30
+                }}>
+                <ReactPaginate
+                breakLabel="..."
+                nextLabel="next >"
+                onPageChange={handlePageClick}
+                pageRangeDisplayed={3}
+                pageCount={pageCount}
+                previousLabel="< prev"
+                renderOnZeroPageCount={null}
+                containerClassName={"pagination"}
+                previousLinkClassName={"pagination__link"}
+                nextLinkClassName={"pagination__link"}
+                disabledClassName={"pagination__link--disabled"}
+                activeClassName={"pagination__link--active"}
+                />
+                </div>
             </Container>
         </div>
+
+        <ExampleModal 
+        show = {modalShow}
+        word={modalWord}
+        examples={modalExample}
+        onHide = {() => setModalShow(false)}/>
         </>
     )
 }
